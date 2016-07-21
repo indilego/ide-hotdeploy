51aa1acebb80be104113a9f4f7023f2ba3b2977f
alejandro.blasco <alejandro.blasco@MW7YQF13AH91US.dir.svc.accenture.com>
2016-07-21 10:19:08 +0200

    f

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -8,9 +8,17 @@ public class HelloController {
     
     @RequestMapping("/")
     public String index() {
-    	String a="Hola";
-    	String b="adios";
-        return a+b+"Cambiado 1";
+    	String a="Hola ";
+    	int z=0;
+    	//String salida=llamada("ok");
+    	String salida="1";
+    	String b="Adios 1111";
+        return salida+z+a+b+"Cambiado 2";
     }
-    
+
+	/*private String llamada(String string) {
+		// TODO Auto-generated method stub
+		return"ko";
+	}*/
+
 }
